1394b4b32e55dcb8909c44f55ba8597ad63d362d
Dowie <dowied4@mymacewan.ca>
2019-03-24 03:01:49 -0600

    Add comments

--- valid_moves.c
+++ valid_moves.c
@@ -1,3 +1,10 @@
+/* Author: Aidan Bush, David Dowie, Ben Ha
+ * Project: Konane puzzle
+ * Course: CMPT 355
+ * Date: Mar. 24, 19
+ * File: valid_moves.c
+ * Description: generates the valid moves for a given state based off a colour of stone.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -64,11 +71,13 @@ int valid_moves(char **cur_state, search_type move_set)
 {
   int i, j, ind = 0;
   char search_char; //whether we are looking for the available moves for white or black
+  //allocate memory
   char **temp_state = malloc(sizeof(char *) * MAX_ROW);
   for (i = 0; i < MAX_ROW; i++)
   {
     temp_state[i] = malloc(sizeof(char) * MAX_COL);
   }
+  //assign a temporary state with the current state
   temp_state = copy_state(cur_state);
 
   //Restricted set of possible moves made for initial move
@@ -87,7 +96,7 @@ int valid_moves(char **cur_state, search_type move_set)
     temp_state = copy_state(cur_state);
     temp_state[4][3] = EMPTY_SPACE;
   }
-
+  //assign which colour of stone we are evaluating based off of
   if (move_set == SEARCH_BLACK)
   {
     search_char = STONE_BLACK;
@@ -96,6 +105,7 @@ int valid_moves(char **cur_state, search_type move_set)
   {
     search_char = STONE_WHITE;
   }
+
   //Search through state for possible moves based off search_char
   temp_state = copy_state(cur_state);
   for (i = 0; i < MAX_ROW; i++)
@@ -105,17 +115,21 @@ int valid_moves(char **cur_state, search_type move_set)
       //Evaluate based on search_char
       if (temp_state[i][j] == search_char)
       {
-        //check down
+        //Ensure we dont exceed the bounds of the array
         if ((i + 2 < MAX_ROW))
         {
+          //ensure the adjacent space is not empty
           if (temp_state[i + 1][j] != EMPTY_SPACE)
           {
+            //check target space if its empty
             if (temp_state[i + 2][j] == EMPTY_SPACE)
             {
+              //display the resulting move
               temp_state[i + 2][j] = search_char;
-              temp_state[i][j] = EMPTY_SPACE;
+              temp_state[i][j] = EMPTY_SPACE; //remove the oppositions stone
               temp_state[i + 1][j] = EMPTY_SPACE;
               print_state(temp_state); //ADD CHILD HERE
+              //check for double move
               if ((i + 4 < MAX_ROW))
               {
                 if (temp_state[i + 3][j] != EMPTY_SPACE)
