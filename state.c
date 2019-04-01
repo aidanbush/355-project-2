40afd0eb655d23a5eac1e19470eb8b80a9f2164f
Aidan Bush <aidandbush@gmail.com>
2019-03-31 18:42:30 -0600

    Update printing of move

--- state.c
+++ state.c
@@ -53,8 +53,7 @@ void print_move(state_s *state) {
     if (state == NULL)
         return;
     if (state->move.end_row == DEFAULT_MOVE_POS) {
-        printf("Remove From: %c%d\n",'A' + state->move.start_col, BOARD_SIZE - state->move.start_row);
-        //printf("Remove From: %c%d\n",'A' + state->move.start_row, state->move.start_col);
+        printf("%c%d\n",'A' + state->move.start_col, BOARD_SIZE - state->move.start_row);
     } else {
         // print location
         printf("%c%d-%c%d\n", 'A' + state->move.start_col, BOARD_SIZE - state->move.start_row,
